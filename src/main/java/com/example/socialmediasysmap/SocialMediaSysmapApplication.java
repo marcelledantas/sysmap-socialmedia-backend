fdb913a00a737058d78ee2f3f8acffd3ad8a8558
marcelledantas <marcellemoraesdantas@gmail.com>
2023-05-01 23:43:25 -0300

    Starting the project

---
+++ src/main/java/com/example/socialmediasysmap/SocialMediaSysmapApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialmediasysmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaSysmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaSysmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socialmediasysmap/SocialMediaSysmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialmediasysmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaSysmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
