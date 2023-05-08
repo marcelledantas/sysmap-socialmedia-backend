05815fad8490cfadfd378c40db2af45647195a71
marcelledantas <marcellemoraesdantas@gmail.com>
2023-05-08 02:16:02 -0300

    Modify: controller user

--- src/main/java/com/example/socialmediasysmap/controller/UserController.java
+++ src/main/java/com/example/socialmediasysmap/controller/UserController.java
@@ -1,10 +1,90 @@
 package com.example.socialmediasysmap.controller;
 
+import com.example.socialmediasysmap.model.Post;
+import com.example.socialmediasysmap.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.created(URI.create("/users/" + createdUser.getId())).body(createdUser);
+    }
+
+    @PostMapping("/{userId}/follow")
+    public ResponseEntity<Void> followUser(@PathVariable Long userId) {
+        userService.followUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{userId}/unfollow")
+    public ResponseEntity<Void> unfollowUser(@PathVariable Long userId) {
+        userService.unfollowUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{userId}/posts")
+    public ResponseEntity<List<Post>> getAllPostsForUser(@PathVariable Long userId) {
+        List<Post> posts = userService.getAllPostsForUser(userId);
+        return ResponseEntity.ok(posts);
+    }
+
+    @GetMapping("/{userId}/followers")
+    public ResponseEntity<List<User>> getFollowersForUser(@PathVariable Long userId) {
+        List<User> users = this.userService.getFollowersFromUser(userId);
+        return ResponseEntity.ok(posts);
+    }
+
+    @GetMapping("/users/profile")
+    public ResponseEntity<ResponseObjectService> findById(@RequestBody IdObjectEntity inputId) {
+        return new ResponseEntity<ResponseObjectService>(userService.findById(inputId.getId()), HttpStatus.OK);
+    }
+
+    @PostMapping("/users/save")
+    public ResponseEntity<ResponseObjectService> saveUser(@RequestBody UserEntity inputUser) {
+        return new ResponseEntity<ResponseObjectService>(userService.saveUser(inputUser), HttpStatus.OK);
+    }
+
+    @PostMapping("/users/signin")
+    public ResponseEntity<ResponseObjectService> userSignIn(@RequestBody UserSignInEntity inputUser) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(inputUser.getEmail(), inputUser.getPassword()));
+            String token = jwtUtil.generateToken(inputUser.getEmail());
+
+            Optional<UserEntity> optUser = userRepo.findByEmail(inputUser.getEmail());
+            UserEntity user = optUser.get();
+            user.setPassword("");
+            return new ResponseEntity<ResponseObjectService>(new ResponseObjectService("success", "authenticated", new AuthorizedEntity(user, token)), HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<ResponseObjectService>(new ResponseObjectService("fail", "unauthenticated", null), HttpStatus.OK);
+        }
+    }
+
+
+    @PutMapping("/users/update")
+    public ResponseEntity<ResponseObjectService> update(@RequestBody UserEntity inputUser) {
+        return new ResponseEntity<ResponseObjectService>(userService.update(inputUser), HttpStatus.OK);
+    }
+
+    //findAllUsers
+    //
 }
