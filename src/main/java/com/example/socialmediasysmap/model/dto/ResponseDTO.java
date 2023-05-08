df63b1f69e0bbbf5d739622b628356560c383460
marcelledantas <marcellemoraesdantas@gmail.com>
2023-05-08 01:29:32 -0300

    Add: response dto

---
+++ src/main/java/com/example/socialmediasysmap/model/dto/ResponseDTO.java
@@ -0,0 +1,25 @@
+package com.example.socialmediasysmap.model.dto;
+
+public class ResponseDTO {
+    private String status;
+    private String message;
+
+    public ResponseDTO(String status, String message, Object payload) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
