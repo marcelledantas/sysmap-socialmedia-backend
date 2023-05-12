f38f93b63205f5d5a37d931b111fde1e103a4398
marcelledantas <marcellemoraesdantas@gmail.com>
2023-05-12 20:57:29 -0300

    Add: security configuration class

---
+++ src/main/java/com/example/parrotsysmap/security/ApplicationSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.parrotsysmap.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
