2d8dce0a81be5466e6a9dec73a291564a6e5e8ee
marcelledantas <marcellemoraesdantas@gmail.com>
2023-05-13 01:51:15 -0300

    Add: security configuration class

--- src/main/java/com/example/parrotsysmap/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/parrotsysmap/security/ApplicationSecurityConfig.java
@@ -1,28 +1,53 @@
 package com.example.parrotsysmap.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig{
 
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.
-                authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
-                .httpBasic(withDefaults());
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests().requestMatchers("/Admin/**").hasRole("ADMIN")
+                .requestMatchers("/", "index", "/css/*", "/js*").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        UserDetails userAnna = User.builder()
+                .username("Anna")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(userAnna);
     }
 
 }
